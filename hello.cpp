617420d23486817d31885e99b0449f22d9518cf5
Vidhathri V <144589117+vvidha3@users.noreply.github.com>
2025-03-12 01:38:48 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "Modified"
+  cout << "Modified";
   return 0;
 }
